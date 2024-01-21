a65c62c81987a5243bb1dafb2326d60a1fdbbb61
anastasiiaipe <143907938+anastasiiaipe@users.noreply.github.com>
2024-01-21 15:12:19 +0100

    task-4

--- js/lesson-4_7-8.js
+++ js/lesson-4_7-8.js
@@ -30,3 +30,21 @@
 //   });
 // }
 // console.log(items);
+
+//TODO: № 4 ==============================================
+// Кнопка "Приховати" ховає текст і замінює назву кнопки на
+// "Розкрити", при повторному натисканні текст знову стає доступним
+// і кнопка набуває початкового вигляду.
+
+// const btn = document.querySelector("#passwordButton");
+// const input = document.querySelector("#passwordInput");
+// btn.addEventListener("click", onHidePassword);
+// function onHidePassword() {
+//   const typeInput = input.getAttribute("type");
+//   const toggleTypeInput = typeInput === "password" ? "text" : "password";
+//   input.setAttribute("type", toggleTypeInput);
+//   const textBtn = btn.textContent;
+//   const toggleTextBtn = textBtn === "Приховати" ? "Розкрити" : "Приховати";
+//   btn.textContent = toggleTextBtn;
+//   console.log(toggleTextBtn);
+// }
