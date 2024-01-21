ceabb2c456f8e10f4e85d0f2b31a4b87a0e71a11
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-01-21 15:01:55 +0200

    +

---
+++ js/lesson-4_7-8.js
@@ -0,0 +1,7 @@
+//TODO: № 1 ==============================================
+// Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
+// const btn = document.querySelector("#alertButton");
+// btn.addEventListener("click", () => {
+//   const input = document.querySelector("#alertInput");
+//   console.log(input.value);
+// });
